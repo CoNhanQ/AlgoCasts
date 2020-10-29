fe77ec4b52beb996aae2ff5cbcc33f876137d4ef
Co Nhan <conhanq@gmail.com>
2020-10-28 22:43:59 -0700

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  let max = 0;
+  let maxchar = '';
 
+
+  for (let char of str) {
+      if (!chars[char]) {
+          chars[char] = 1;
+      } else {
+          chars[char]++;
+      }
+  }
+  
+  for (let char in chars) {
+      if (chars[char] > max) {
+          max = chars[char];
+          maxchar = char;
+      }
+  }
+  return maxchar
+}
 module.exports = maxChar;
